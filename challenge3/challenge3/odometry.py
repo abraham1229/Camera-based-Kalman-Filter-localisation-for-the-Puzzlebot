94a4b797b8f06517f9f5d6eb80fee69ce8aedadd
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-11 16:46:34 -0500

    Se agrega paquete para challenge3

---
+++ challenge3/challenge3/odometry.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from challenge3.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ challenge3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'challenge3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.stl'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='abraham1229',
+    maintainer_email='abrahamortizcastro1229@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'odometry = challenge3.odometry:main',
+            'puzzlebot = challenge3.puzzlebot:main'
+        ],
+    },
+)
