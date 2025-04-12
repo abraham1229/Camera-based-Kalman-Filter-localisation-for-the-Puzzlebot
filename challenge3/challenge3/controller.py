12fe4ae830e7e815be5f1b7080c5dce3a2fbebce
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-11 21:19:08 -0500

    Se agrega condicion de distancia para probar funcion en conjunto

--- challenge3/challenge3/controller.py
+++ challenge3/challenge3/controller.py
@@ -18,11 +18,33 @@ class Controller(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info('Controller node initialized')
 
+        self.subscription_odometry = self.create_subscription(
+            Vector,
+            'odometria',
+            self.callback_odometry,
+            rclpy.qos.qos_profile_sensor_data ) #Se debe de incluir la lectura de datos
+        
+
+        self.Posx = 0.0
+        self.Posy = 0.0
+        self.Postheta = 0.0
+
     def timer_callback(self):
         twist_msg = Twist()
-        twist_msg.linear.x = 0.01
+        
+        if self.Posx >= 0.15:
+            twist_msg.linear.x = 0.0
+        else:
+            twist_msg.linear.x = 0.01
+
         twist_msg.angular.z = 0.0
         self.pub_cmd_vel.publish(twist_msg)
+
+    def callback_odometry(self, msg):
+        if msg is not None:
+            self.Posx = msg.x
+            self.Posy = msg.y
+            self.Postheta = msg.theta
 
 
 def main(args=None):
