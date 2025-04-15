bf72fadb8d026c157881da8fca955fd418a2ce40
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-15 12:40:59 -0500

    Se agregan trayectorias

--- challenge3/challenge3/path_generator.py
+++ challenge3/challenge3/path_generator.py
@@ -26,26 +26,83 @@ class My_Talker_Params(Node):
     def timer_callback(self):
         ri_type = self.get_parameter('type').get_parameter_value().integer_value
  
-        if ri_type == 0:
-            # Square condition
+        if ri_type == 1:
+            # Triángulo
+            self.msg.x1 = 0.5
+            self.msg.y1 = 0.87
+            self.msg.x2 = 1.0
+            self.msg.y2 = 0.0
+            self.msg.x3 = 0.0
+            self.msg.y3 = 0.0
+            self.msg.x4 = 0.0
+            self.msg.y4 = 0.0
+            self.msg.x5 = 0.0
+            self.msg.y5 = 0.0
+            self.msg.x6 = 0.0
+            self.msg.y6 = 0.0
+            self.msg.x7 = 0.0
+            self.msg.y7 = 0.0
+            self.msg.x8 = 0.0
+            self.msg.y8 = 0.0
+
+        if ri_type == 2:
+            # Cuadrado
             self.msg.x1 = 1.0
-            self.msg.y1 = 1.0
+            self.msg.y1 = 0.0
             self.msg.x2 = 1.0
             self.msg.y2 = 1.0
-            self.msg.x3 = 1.0
+            self.msg.x3 = 0.0
             self.msg.y3 = 1.0
-            self.msg.x4 = 1.0
-            self.msg.y4 = 1.0
-        
-        if ri_type == 1:
-            self.msg.x1 = 2.0
-            self.msg.y1 = 2.0
-            self.msg.x2 = 2.0
-            self.msg.y2 = 2.0
-            self.msg.x3 = 2.0
-            self.msg.y3 = 2.0
-            self.msg.x4 = 2.0
-            self.msg.y4 = 2.0
+            self.msg.x4 = 0.0
+            self.msg.y4 = 0.0
+            self.msg.x5 = 0.0
+            self.msg.y5 = 0.0
+            self.msg.x6 = 0.0
+            self.msg.y6 = 0.0
+            self.msg.x7 = 0.0
+            self.msg.y7 = 0.0
+            self.msg.x8 = 0.0
+            self.msg.y8 = 0.0
+
+        if ri_type == 3:
+            # Pentágono
+            self.msg.x1 = 0.35
+            self.msg.y1 = 0.02
+            self.msg.x2 = 0.9
+            self.msg.y2 = 0.21
+            self.msg.x3 = 0.9
+            self.msg.y3 = 0.59
+            self.msg.x4 = 0.35
+            self.msg.y4 = 0.98
+            self.msg.x5 = 0.0
+            self.msg.y5 = 0.5
+            self.msg.x6 = 0.35
+            self.msg.y6 = 0.02
+            self.msg.x7 = 0.0
+            self.msg.y7 = 0.0
+            self.msg.x8 = 0.0
+            self.msg.y8 = 0.0
+            
+
+        if ri_type == 4:
+            # Hexágono
+            self.msg.x1 = 0.25
+            self.msg.y1 = 0.07
+            self.msg.x2 = 0.75
+            self.msg.y2 = 0.07
+            self.msg.x3 = 1.0
+            self.msg.y3 = 0.5
+            self.msg.x4 = 0.75
+            self.msg.y4 = 0.93
+            self.msg.x5 = 0.25
+            self.msg.y5 = 0.93
+            self.msg.x6 = 0.0
+            self.msg.y6 = 0.05
+            self.msg.x7 = 0.25
+            self.msg.y7 = 0.07
+            self.msg.x8 = 0.0
+            self.msg.y8 = 0.0
+
         
 
         self.pub.publish(self.msg)
