d6566985260eb7e6dda69cab07e11e02015fabe7
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-05-24 22:03:05 -0500

    Se agrega esqueleto para robot real

--- final_challenge/launch/puzzlebot_launch.py
+++ final_challenge/launch/puzzlebot_launch.py
@@ -1,48 +1,22 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-
-from launch.actions import LogInfo, RegisterEventHandler
-from launch.event_handlers import OnShutdown
-from launch.substitutions import LocalSubstitution
-
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-  
-  # Parameters
-  config = os.path.join(
-      get_package_share_directory('final_challenge'),
-      'config',
-      'params.yaml'
-      )
-  
-  path_generator_node = Node(
-      package='final_challenge',
-      executable='path_generator',
-      output='screen',
-      parameters=[config]
-  )
     
-  controller_node = Node(name="controller",
-                          package='final_challenge',
-                          executable='controller',
-                          parameters=[config]
-                          )
-  
-  odometry_node = Node(name="odometry",
-                          package='final_challenge',
-                          executable='odometry',
-                          parameters=[config]
-                          )
 
+    control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('final_challenge'),
+                'launch',
+                'control_launch.py'
+            )
+        )
+    )
 
-  l_d = LaunchDescription([
-      controller_node,
-      odometry_node,
-      path_generator_node,
-      ])
-
-  return l_d
+    return LaunchDescription([
+        control
+    ])
