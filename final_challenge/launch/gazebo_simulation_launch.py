e7c3ab38d563cfbeda806f93d1d01deea7dadcb0
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-05-24 21:36:36 -0500

    Se agrega launcher para gazebo

--- final_challenge/launch/gazebo_simulation_launch.py
+++ final_challenge/launch/gazebo_simulation_launch.py
@@ -1,6 +1,8 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 from launch.actions import LogInfo, RegisterEventHandler
@@ -9,6 +11,18 @@ from launch.substitutions import LocalSubstitution
 
 
 def generate_launch_description():
+  
+
+  # GAZEBO SIM
+  bringup_simple_simulation = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+          os.path.join(
+              get_package_share_directory('puzzlebot_gazebo'),
+              'launch',
+              'bringup_simulation_simple_launch.py'
+          )
+      )
+  )
 
   init_x = 0.0
   init_y = 0.0
@@ -74,6 +88,7 @@ def generate_launch_description():
                               )
 
   l_d = LaunchDescription([
+      bringup_simple_simulation,
       controller_node,
       odometry_node,
       path_generator_node,
