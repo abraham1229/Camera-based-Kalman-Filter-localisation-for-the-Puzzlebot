ad328c573e39181a687904c93b3c5afa14d7ddd3
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-05-25 16:08:39 -0500

    Se agrega propuesta de launcher para puzzlebot real

--- final_challenge/launch/puzzlebot_launch.py
+++ final_challenge/launch/puzzlebot_launch.py
@@ -5,8 +5,37 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    
 
+    hackerboard = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('puzzlebot_ros'),
+                'launch',
+                'micro_ros_agent.launch.py'
+            )
+        )
+    )
+
+    lidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('rplidar_ros'),
+                'launch',
+                'view_rplidar_a1_launch.py'
+            )
+        )
+    )
+
+    camera = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('puzzlebot_ros'),
+                'launch',
+                'camera_jetson.launch.py'
+            )
+        )
+    )
+    
     control = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
@@ -18,5 +47,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        hackerboard,
+        lidar,
+        camera,
         control
     ])
