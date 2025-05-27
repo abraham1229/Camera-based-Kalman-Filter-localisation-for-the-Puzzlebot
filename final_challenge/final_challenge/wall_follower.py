dd98d32e4f13fc6762c8cd49a20115a7d9ec1bac
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-05-27 14:38:37 -0500

    Se hace wall follower para bot real

--- final_challenge/final_challenge/wall_follower.py
+++ final_challenge/final_challenge/wall_follower.py
@@ -14,10 +14,10 @@ class WallFollowerLidar(Node):
 
         # Parámetros de control
         self.kp = 1.0
-        self.wall_desired = 0.5      # distancia deseada a la pared derecha
-        self.linear_velocity = 0.1
+        self.wall_desired = 0.3      # distancia deseada a la pared derecha
+        self.max_linear = 0.1
         self.max_angular = 0.2
-        self.threshold_front = 0.5   # si algo está más cerca, se considera obstáculo
+        self.threshold_front = 0.4   # si algo está más cerca, se considera obstáculo
 
         self.get_logger().info("Wall Follower with LiDAR node started")
 
@@ -48,21 +48,24 @@ class WallFollowerLidar(Node):
     def scan_callback(self, msg: LaserScan):
         dist_right = self.get_distance_at_angle(msg, -90)
         dist_right_45 = self.get_distance_at_angle(msg, -45)
-        dist_right_side = np.mean([dist_right, dist_right_45])
+        dist_right_side = min([dist_right, dist_right_45])
         dist_front = self.get_distance_at_angle(msg, 0)
         dist_front_5 = self.get_distance_at_angle(msg, -15)
-        dist_front_mean = np.mean([dist_front, dist_front_5])
+        dist_front_mean = min([dist_front, dist_front_5])
 
-        self.get_logger().info(f"Distancia derecha: {dist_right_side:.2f}")
         self.get_logger().info(f"Distancia frente: {dist_front_mean:.2f}")
+        self.get_logger().info(f"Distancia right: {dist_right_side:.2f}")
 
         twist = Twist()
 
         if dist_front_mean > self.threshold_front:
             error = dist_right_side - self.wall_desired
             turn_rate = -error * self.kp
-            twist.linear.x = self.linear_velocity
+            twist.linear.x = self.max_linear
             twist.angular.z = turn_rate
+
+            if abs(error) > 0.9: #to make sure it will reach the point
+                twist.linear.x = self.max_linear / 5 
 
             # Diagnóstico de giro
             self.get_logger().info(f"Error: {error}")
@@ -70,7 +73,12 @@ class WallFollowerLidar(Node):
             # Obstáculo al frente, detener avance y girar a la izquierda
             twist.linear.x = 0.0
             twist.angular.z = self.max_angular
-            self.get_logger().info("Frente")
+            # self.get_logger().info("Frente")
+
+    
+        
+        twist.linear.x = max(min(twist.linear.x, self.max_linear), -self.max_linear)
+        twist.angular.z = max(min(twist.angular.z, self.max_angular), -self.max_angular)
 
         # Publicar comando final
         self.publisher_.publish(twist)
