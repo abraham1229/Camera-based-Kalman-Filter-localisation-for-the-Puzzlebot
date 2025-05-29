4a88dd06c568bf2094c6d85f6029cd821000c878
AIF31 <a01736001@tec.com>
2025-05-29 15:51:05 -0600

    Last modifications

--- final_challenge/launch/control_launch.py
+++ final_challenge/launch/control_launch.py
@@ -32,17 +32,23 @@ def generate_launch_description():
         parameters=[config]
     )
 
+    #aruco_node = Node(
+    #    package='aruco_opencv',
+    #    executable='aruco_tracker_autostart',
+    #    parameters=[
+    #            {"cam_base_topic": "/camera"},
+    #            {"marker_size": 0.14},
+    #            {"marker_dict": "4X4_50"},
+    #            {"publish_tf": True}
+    #    ],
+    #    remappings=[],
+    #    output='screen'
+    #)
+
     aruco_node = Node(
-        package='aruco_opencv',
-        executable='aruco_tracker_autostart',
-        parameters=[
-                {"cam_base_topic": "/camera"},
-                {"marker_size": 0.14},
-                {"marker_dict": "4X4_50"},
-                {"publish_tf": True}
-        ],
-        remappings=[],
-        output='screen'
+        name='aruco',
+        package='final_challenge',
+        executable='aruco',
     )
 
     rqt_image_view_node = Node(
@@ -70,6 +76,5 @@ def generate_launch_description():
         controller_node,
         odometry_node,
         aruco_node,
-        rqt_image_view_node,
         rviz_node
     ])
